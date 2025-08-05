b6b77035850e493e389bfc3f99676d95c5e52c32
TambyGit <hei.tamby@gmail.com>
2025-08-05 15:31:46 +0300

    feat: base de code

---
+++ test/MainTest.java
@@ -0,0 +1,10 @@
+package test;
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    void testMain() {
+        assertEquals("Hello World", "Hello World");
+    }
+}
