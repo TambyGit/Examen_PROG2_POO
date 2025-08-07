13c4b53052d369f8605228d629f0bc18524184e9
TambyGit <hei.tamby@gmail.com>
2025-08-07 11:54:50 +0300

    feat: All Features with test

--- src/LigueDeCombattant.java
+++ src/LigueDeCombattant.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class LigueDeCombattant {
     private String nom;
     private List<Joueurs> combattants;
-    private List<match> matchs;
+    private List<Match> matchs;
 
     public LigueDeCombattant(String nom) {
         this.nom = nom;
@@ -15,13 +15,13 @@ public class LigueDeCombattant {
     }
 
     public void CreerMatch(int id, LocalDate date, String endroit, TypeMatch type, Joueurs combattant1, Joueurs combattant2) {
-        match nouveauMatch = new match(id, date, endroit, type, combattant1, combattant2);
+        Match nouveauMatch = new Match(id, date, endroit, type, combattant1, combattant2);
         this.matchs.add(nouveauMatch);
     }
 
-    public List<match> getMatchsByFighter(Joueurs combattant) {
-        List<match> result = new ArrayList<>();
-        for (match m : this.matchs) {
+    public List<Match> getMatchsByFighter(Joueurs combattant) {
+        List<Match> result = new ArrayList<>();
+        for (Match m : this.matchs) {
             if (m.getCombattant1().equals(combattant) || m.getCombattant2().equals(combattant)) {
                 result.add(m);
             }
